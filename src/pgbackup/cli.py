0ad3006d1e399c817f518c07cb0bbfd81ad476d9
adachuk <adachukwuemeka93@gmail.com>
2020-07-13 22:53:16 -0500

    implimented S3 interactions/Library/Frameworks/Python.framework/Versions/3.8/bin/python3

--- src/pgbackup/cli.py
+++ src/pgbackup/cli.py
@@ -17,3 +17,18 @@ def create_parser():
             action=DriverAction,
             required=True)
     return parser
+
+
+def main():
+    import boto3
+    from pgbackup import pg_dump, storage
+
+    args = create_parser().parse_args()
+    dump = pg_dump.dump(args.url)
+    if args.driver == 's3':
+        client = boto3.client('s3')
+        # TODO: create a better name based on the database name and the date
+        storage.s3(client, dump.stdout, args.destination, 'example.sql')
+    else:
+        outfile = open(args.destination, 'wb')
+        storage.local(dump.stdout, outfile)
