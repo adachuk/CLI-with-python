c865fa5cbf76b62f906c249912bf2b7a34e6faa1
adachuk <adachukwuemeka93@gmail.com>
2020-07-14 13:41:55 -0500

    Add required python version

--- src/pgbackup/cli.py
+++ src/pgbackup/cli.py
@@ -18,17 +18,24 @@ def create_parser():
             required=True)
     return parser
 
-
 def main():
     import boto3
+    import time
     from pgbackup import pg_dump, storage
 
     args = create_parser().parse_args()
     dump = pg_dump.dump(args.url)
     if args.driver == 's3':
         client = boto3.client('s3')
+        timestamp = time.strftime("%Y-%m-%dT%H:%M", time.localtime())
+        file_name = pg_dump.dump_file_name(args.url, timestamp)
+        print(f"Backing database up to {args.destination} in S3 as {file_name}")
+        storage.s3(client,
+                dump.stdout,
+                args.destination,
+                file_name)
         # TODO: create a better name based on the database name and the date
-        storage.s3(client, dump.stdout, args.destination, 'example.sql')
     else:
         outfile = open(args.destination, 'wb')
+        print(f"Backing database up locally to {outfile.name}")
         storage.local(dump.stdout, outfile)
